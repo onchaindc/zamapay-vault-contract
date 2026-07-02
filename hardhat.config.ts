4c343f469cbcc5c173f98ed4b630b2029db87b6b
onchaindc <onchaindc@users.noreply.github.com>
2026-07-02 21:14:37 +0100

    Use Alchemy RPC for Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ import "./tasks/FHECounter";
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
 const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const ALCHEMY_RPC_URL = vars.get("ALCHEMY_RPC_URL", "");
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -55,7 +55,7 @@ const config: HardhatUserConfig = {
         count: 10,
       },
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: ALCHEMY_RPC_URL,
     },
   },
   paths: {
